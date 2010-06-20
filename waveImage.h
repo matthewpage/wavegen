89fc41cfb71862e4e96e429b9fa1abc3f0557750
matthewpage <github@8notewasteland.com>
2010-06-20 23:26:53 +0100

    removed old struct

--- waveImage.h
+++ waveImage.h
@@ -2,26 +2,12 @@
  * waveImage.h
  *
  *  Created on: 25 May 2010
- *      Author: matthew
+ *  Last Update: 20 June 2010
+ *  Author: Matthew Page
  */
 
 #ifndef WAVEIMAGE_H_
 #define WAVEIMAGE_H_
-
-/*
-typedef struct wf_t
-{
-	char* outputPath;
-	char* baseFilename;
-	int peaksPerSecond;
-	int secondsPerFile;
-	int imageHeight;
-	int imageWidth;
-	int waveHeight;
-	int secondsPerTick;
-	char* startTime;
-} wf_t;
-*/
 
 void initialiseTime(char* startTime);
 void updateImageFile(bool finished);
